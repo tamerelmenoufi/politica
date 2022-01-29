6f84a2c1d52d62624811cb7bce63745c7dd18034
tamerelmenoufi <tamer@mohatron.com.br>
2022-01-29 14:01:36 -0400

    Aplicação das permissões

--- painel/header.php
+++ painel/header.php
@@ -173,7 +173,7 @@
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                  aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#" url="cadastros/usuarios/perfil.php">
+                <a class="dropdown-item" href="#" url="paginas/cadastros/usuarios/perfil.php">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Perfil
                 </a>
