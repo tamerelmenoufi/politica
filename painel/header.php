b0fa0ac48ea13837851395e9efc001e3f15a36c8
tamerelmenoufi <tamer@mohatron.com.br>
2022-01-29 14:11:08 -0400

    Aplicação das permissões

--- painel/header.php
+++ painel/header.php
@@ -165,7 +165,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['usuario']['usuario']; ?></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['usuario']['nome']; ?></span>
                 <i class="fa-solid fa-circle-user"></i>
                 <!--<img class="img-profile rounded-circle"
                      src="#">-->
