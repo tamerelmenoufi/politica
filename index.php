b9e3aa067421978c3bebdc4a7e1f009dea2b54d7
Tamer Elmenoufi <tamer@mohatron.com.br>
2022-02-01 09:58:18 -0400

    Atualização Header

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include "lib/includes.php";
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Politica</title>
+    <title>Politica TESTE</title>
     <meta charset="UTF-8">
     <meta name="description" content="Sistema de Gerenciamento">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
