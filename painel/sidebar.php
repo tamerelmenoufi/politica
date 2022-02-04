02f04b16e4fb9a6d66677d45f8e4a37cec4f1d1f
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-03 22:37:27 -0400

    atualizações cadastro Assessores

--- painel/sidebar.php
+++ painel/sidebar.php
@@ -5,7 +5,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">POLITICA</div>
+        <div class="sidebar-brand-text mx-3" title="Sistema de Gestão Política">SGP</div>
     </a>
 
     <!-- Divider -->
