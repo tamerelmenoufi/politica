d0a392b4af1ca48762d9f39a64b106520968ee7c
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-05 15:03:46 -0400

    atualizações cadastro Eventos

--- painel/paginas/cadastros/oficios/form.php
+++ painel/paginas/cadastros/oficios/form.php
@@ -55,7 +55,29 @@ if ($codigo) {
 }
 
 ?>
-
+<style>
+    div[NovoAssessorBG]{
+        position:fixed;
+        left:0;
+        bottom:0;
+        width:100%;
+        height:100%;
+        z-index:999;
+        background-color:#333;
+        opacity:0.5;
+        display:none;
+        z-index:998;
+    }
+    div[NovoAssessor]{
+        position:relative;
+        z-index:999;
+        background-color:#fff;
+        padding:20px;
+        padding:20px;
+        border-radius:10px;
+        display:none;
+    }
+</style>
 <nav aria-label="breadcrumb">
     <ol class="breadcrumb shadow bg-gray-custom">
         <li class="breadcrumb-item"><a href="#" url="content.php">Início</a></li>
@@ -90,6 +112,7 @@ if ($codigo) {
                         required
                 >
                     <option value=""></option>
+                    <option value="novo">Novo Cadastro</option>
                     <?php
                     $query = "SELECT * FROM assessores ORDER BY nome";
                     $result = mysql_query($query);
@@ -104,6 +127,9 @@ if ($codigo) {
                 </select>
 
             </div>
+            <div NovoAssessorBG></div>
+            <div NovoAssessor></div>
+
 
             <div class="row">
                 <div class="col-md-6">
@@ -253,6 +279,32 @@ if ($codigo) {
                 {caption: '<?= $codigo;?>.pdf', type: "pdf", size: "100%", width: "100%", key: 1},
             ],
         });
+
+        $("#assessor").change(function(){
+            valor = $(this).val();
+            if(valor === 'novo'){
+                $.ajax({
+                    url:"paginas/cadastros/assessores/novo.php",
+                    success:function(dados){
+                        $("div[NovoAssessor]").html(dados);
+                        $("div[NovoAssessorBG]").css("display","block");
+                        $("div[NovoAssessor]").css("display","block");
+                    },
+                    error:function(){
+                        alert('Ocorreu um erro!');
+                    }
+                });
+            }
+        });
+
+        $("div[NovoAssessorBG]").click(function(){
+            $("div[NovoAssessorBG]").css("display","none");
+            $("div[NovoAssessor]").css("display","none");
+            $("div[NovoAssessor]").html('');
+            $("#assessor").val('');
+            $("#assessor").selectpicker('refresh');
+        });
+
 
         $("#assessor").selectpicker();
 
