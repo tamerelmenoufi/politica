1514633b377bcf821c76db7c55100d53d6cd24ad
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-06 13:11:08 -0400

    Inclusão do cadastro dos Relatórios

--- painel/paginas/relatorios/index.php
+++ painel/paginas/relatorios/index.php
@@ -1,6 +1,35 @@
 <?php
     include "../../../lib/includes.php";
     $md5  = md5(date("YmdHis"));
+
+    $bg = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ];
+
+    $bd = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+
+
+    $query = "select b.tipo, count(a.*) as qt from servicos a left join servico_tipo b on a.tipo = b.codigo group by a.tipo";
+    $result = mysql_query($query);
+    while($d = mysql_fetch_object($result)){
+        $rotulo[] = $d->tipo;
+        $qt[] =  $d->qt;
+    }
+    $rotulo = "'".implode("', '",$rotulo)."'";
+    $qt = implode(", ",$qt);
+
 ?>
 
 <div class="row">
@@ -21,10 +50,10 @@ const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
 const myChart<?=$md5?> = new Chart(ctx<?=$md5?>, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [<?=$rotulo?>],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [<?=$qt?>],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
