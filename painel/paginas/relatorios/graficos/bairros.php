8fdabc66da955a8ef88d6fefd9509cb33fab011c
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-07 10:27:19 -0400

    Inclusão do cadastro dos Relatórios

--- painel/paginas/relatorios/graficos/bairros.php
+++ painel/paginas/relatorios/graficos/bairros.php
@@ -43,6 +43,36 @@
 <h5>Relatório Por Bairro</h5>
 <canvas id="myChart<?=$md5?>" width="400" height="400"></canvas>
 
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
 <script>
 
 const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
