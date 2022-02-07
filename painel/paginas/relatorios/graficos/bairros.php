a2e2b8ea2387d7c93b5f82f8cb5ce403b8d943a7
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-07 09:42:03 -0400

    Inclusão do cadastro dos Bairros

---
+++ painel/paginas/relatorios/graficos/bairros.php
@@ -0,0 +1,125 @@
+<?php
+    include "../../../../lib/includes.php";
+    $md5  = md5(date("YmdHis").$_SERVER["PHP_SELF"]);
+
+    $Legenda = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+    $Bg = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ];
+
+    $Bd = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+
+
+    $query = "select b.tipo, count(*) as qt from servicos a left join servico_tipo b on a.tipo = b.codigo group by a.tipo";
+    $result = mysql_query($query);
+    $i=0;
+    while($d = mysql_fetch_object($result)){
+        $rotulo[] = $d->tipo;
+        $qt[] =  $d->qt;
+        $lg[] = $Legenda[$i];
+        $bg[] = $Bg[$i];
+        $bd[] = $Bd[$i];
+    $i++;
+    }
+    $Lg =  "'".implode("', '",$rotulo)."'";
+    $lg = "'".implode("', '",$lg)."'";
+    $qt = implode(", ",$qt);
+?>
+
+
+<h5>Relatório Geral</h5>
+<canvas id="myChart<?=$md5?>" width="400" height="400"></canvas>
+
+<script>
+
+const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
+const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
+{
+  type: 'bar',
+  data: {
+        labels: [<?=$lg?>],
+        datasets: [{
+            label:  [<?=$lg?>],
+            data: [<?=$qt?>],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            rotulos:[<?=$Lg?>]
+        }]
+    },
+  options: {
+    indexAxis: 'y',
+    // Elements options apply to all of the options unless overridden in a dataset
+    // In this case, we are setting the border of each horizontal bar to be 2px wide
+    elements: {
+      bar: {
+        borderWidth: 2,
+      }
+    },
+    responsive: true,
+    plugins: {
+      legend: false/*{
+        position: 'right',
+      }*/,
+      title: {
+        display: true,
+        text: 'Chart.js Horizontal Bar Chart'
+      },
+
+
+
+      tooltip: {
+                callbacks: {
+                    title: function(context){
+                        indx = context[0].parsed.y;
+                        return context[0].dataset.rotulos[indx];
+                    },
+                    label: function(context) {
+                        indx = context.parsed.y;
+                        var label = ' ' + context.dataset.label[indx] || '';
+
+                        if (label) {
+                            label += ' : ';
+                        }
+                        if (context.parsed.y !== null) {
+                            label += context.parsed.x + ' Registro(s)';
+                        }
+                        return label;
+                    }
+                }
+            }
+
+    }
+  },
+}
+
+
+);
+</script>
--- painel/paginas/relatorios/index.php
+++ painel/paginas/relatorios/index.php
@@ -1,48 +1,12 @@
 <?php
     include "../../../lib/includes.php";
-    $md5  = md5(date("YmdHis"));
-
-    $Legenda = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-
-    $Bg = [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-    ];
-
-    $Bd = [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-    ];
-
-
-    $query = "select b.tipo, count(*) as qt from servicos a left join servico_tipo b on a.tipo = b.codigo group by a.tipo";
-    $result = mysql_query($query);
-    $i=0;
-    while($d = mysql_fetch_object($result)){
-        $rotulo[] = $d->tipo;
-        $qt[] =  $d->qt;
-        $lg[] = $Legenda[$i];
-        $bg[] = $Bg[$i];
-        $bd[] = $Bd[$i];
-    $i++;
-    }
-    $Lg =  "'".implode("', '",$rotulo)."'";
-    $lg = "'".implode("', '",$lg)."'";
-    $qt = implode(", ",$qt);
 ?>
-
 <div class="row">
-    <div class="col-12">
-        <h5>Nome do Gráfico 1</h5>
-        <canvas id="myChart<?=$md5?>" width="400" height="400"></canvas>
+<div class="col-6">
+        <div graficos opc="geral"></div>
+    </div>
+    <div class="col-6">
+        <div graficos opc="bairros"></div>
     </div>
 </div>
 
@@ -50,86 +14,15 @@
 
 
 <script>
-
-//const Legendas<?=$md5?> = [<?=$Lg?>];
-
-const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
-const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
-{
-  type: 'bar',
-  data: {
-        labels: [<?=$lg?>],
-        datasets: [{
-            label:  [<?=$lg?>],
-            data: [<?=$qt?>],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1,
-            rotulos:[<?=$Lg?>]
-        }]
-    },
-  options: {
-    indexAxis: 'y',
-    // Elements options apply to all of the options unless overridden in a dataset
-    // In this case, we are setting the border of each horizontal bar to be 2px wide
-    elements: {
-      bar: {
-        borderWidth: 2,
-      }
-    },
-    responsive: true,
-    plugins: {
-      legend: false/*{
-        position: 'right',
-      }*/,
-      title: {
-        display: true,
-        text: 'Chart.js Horizontal Bar Chart'
-      },
-
-
-
-      tooltip: {
-                callbacks: {
-                    title: function(context){
-                        indx = context[0].parsed.y;
-                        return context[0].dataset.rotulos[indx];
-                    },
-                    label: function(context) {
-                        indx = context.parsed.y;
-                        var label = ' ' + context.dataset.label[indx] || '';
-
-                        if (label) {
-                            label += ' : ';
-                        }
-                        if (context.parsed.y !== null) {
-                            label += context.parsed.x + ' Registro(s)';
-                        }
-                        return label;
-                    }
+    $(function(){
+        $("div[graficos]").each(function(){
+            opc = $(this).attr("opc");
+            $.ajax({
+                url:"paginas/relatorios/grafios/"+opc+".php",
+                success:function(dados){
+                     $(this).html(dados);
                 }
-            }
-
-
-
-    }
-  },
-}
-
-
-);
+            });
+        });
+    })
 </script>
