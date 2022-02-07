111f6c8f9fc860dd8990037131a891339194c2a2
raicleysantana <raicleysantana1@gmail.com>
2022-02-06 21:26:49 -0400

    Moficações no modulo de agendamento

--- consulta_agenda/resultado.php
+++ consulta_agenda/resultado.php
@@ -4,52 +4,116 @@ include '../lib/includes.php';
 $data = $_POST['data'];
 $servico_tipo = $_POST['servico_tipo'];
 
+
 $query = "SELECT s.*, b.nome AS b_nome FROM servicos s "
-    . "LEFT JOIN beneficiados b ON b.codigo = s.beneficiado "
-    . "WHERE s.data_agenda LIKE '%{$data}%' AND s.tipo = '{$servico_tipo}' ORDER BY data_agenda";
+    . "LEFT JOIN beneficiados b ON b.codigo = s.beneficiado ";
 
-$result = mysql_query($query);
+$whereGeral = "WHERE s.data_agenda LIKE '%{$data}%' AND s.tipo = '{$servico_tipo}' ORDER BY data_agenda";
+$result = mysql_query($query . $whereGeral);
 
+$mes = date('m', strtotime($data));
+$whereMes = "WHERE DATE_FORMAT(s.data_agenda, \"%m\") = '{$mes}' AND s.tipo = '{$servico_tipo}' ORDER BY data_agenda";
+$resultMes = mysql_query($query . $whereMes);
 
+$mesArray = [
+    '01' => 'Janeiro',
+    '02' => 'Fevereiro',
+    '03' => 'Março',
+    '04' => 'Abril',
+    '05' => 'Maio',
+    '06' => 'Junho',
+    '07' => 'Julho',
+    '08' => 'Agosto',
+    '09' => 'Setembro',
+    '10' => 'Outubro',
+    '11' => 'Novembro',
+    '12' => 'Dezembro'
+]
 ?>
-<h1 class="h4 text-gray-600 my-1 mb-2">
-    <?= date('d/m/Y', strtotime($data)); ?>
-</h1>
 <br>
-<table class="table table-striped table-hover" style="font-size: .9rem;">
-    <thead>
-    <tr>
-        <td>Horário</td>
-        <td>Beneficiado</td>
-        <td>Especialista</td>
-        <td>Contato</td>
-        <td>Situação</td>
-        <td>Ações</td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    if (mysql_num_rows($result)):
-        while ($d = mysql_fetch_object($result)):
-            $data = date('h:i', strtotime($d->data_agenda));
-            ?>
-            <tr>
-                <td><?= $data; ?></td>
-                <td class="text-center"><?= $d->b_nome; ?></td>
-                <td class="text-center"><?= $d->especialista; ?></td>
-                <td><?= $d->contato; ?></td>
-                <td><?= $d->situacao; ?></td>
-                <td>
-                    <button type="button" class="btn btn-sm btn-link">
-                        <i class="fa-regular fa-eye"></i>
-                    </button>
-                </td>
-            </tr>
-        <?php endwhile; ?>
+<h1 class="h5 text-gray-800 my-1 mt-2">
+    <?= date('d', strtotime($data)); ?>/<?= $mesArray[date('m', strtotime($data))]; ?>
+</h1>
 
-    <?php else:
-        echo '<tr><td colspan="6">Nenhum agendamento encontrado</td></tr>';
-        ?>
-    <?php endif; ?>
-    </tbody>
-</table>
+<div class="table-responsive mb-2" style="min-height: 140px;border: 1px solid #e3e6f0">
+
+    <table class="table table-sm table-hover" style="font-size: .9rem;">
+        <thead>
+        <tr>
+            <th class="text-center">Horário</th>
+            <th class="text-center">Beneficiado</th>
+            <th class="text-center">Especialista</th>
+            <th class="text-center">Contato</th>
+            <th class="text-center">Situação</th>
+            <th class="text-center">Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if (mysql_num_rows($result)):
+            while ($d = mysql_fetch_object($result)):
+                $data = date('h:i', strtotime($d->data_agenda));
+                ?>
+                <tr>
+                    <td class="text-center"><?= $data; ?></td>
+                    <td class="text-center"><?= $d->b_nome; ?></td>
+                    <td class="text-center"><?= $d->especialista; ?></td>
+                    <td><?= $d->contato; ?></td>
+                    <td><?= $d->situacao; ?></td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-link">
+                            <i class="fa-regular fa-eye"></i>
+                        </button>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+
+        <?php else:
+            echo '<tr><td colspan="6">Nenhum agendamento encontrado</td></tr>';
+            ?>
+        <?php endif; ?>
+        </tbody>
+    </table>
+</div>
+<br>
+<h1 class="h5 text-gray-800">Mês de <?= $mesArray[date('m', strtotime($data))]; ?></h1>
+
+<div class="table-responsive" style="min-height: 140px;border: 1px solid #e3e6f0">
+    <table class="table table-sm table-hover" style="font-size: .9rem;">
+        <thead>
+        <tr>
+            <th class="text-center">Horário</th>
+            <th class="text-center">Beneficiado</th>
+            <th class="text-center">Especialista</th>
+            <th class="text-center">Contato</th>
+            <th class="text-center">Situação</th>
+            <th class="text-center">Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        if (mysql_num_rows($resultMes)):
+            while ($d = mysql_fetch_object($resultMes)):
+                $data = date('d/m/Y h:i', strtotime($d->data_agenda));
+                ?>
+                <tr>
+                    <td class="text-center"><?= $data; ?></td>
+                    <td class="text-center"><?= $d->b_nome; ?></td>
+                    <td class="text-center"><?= $d->especialista; ?></td>
+                    <td><?= $d->contato; ?></td>
+                    <td><?= $d->situacao; ?></td>
+                    <td>
+                        <button type="button" class="btn btn-sm btn-link">
+                            <i class="fa-regular fa-eye"></i>
+                        </button>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+
+        <?php else:
+            echo '<tr><td colspan="6">Nenhum agendamento encontrado</td></tr>';
+            ?>
+        <?php endif; ?>
+        </tbody>
+    </table>
+</div>
