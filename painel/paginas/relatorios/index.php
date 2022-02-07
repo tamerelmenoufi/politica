d6ff62c9612b86d146844dc30f46a654ffad737f
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-06 21:43:41 -0400

    Inclusão do cadastro dos Relatórios

--- painel/paginas/relatorios/index.php
+++ painel/paginas/relatorios/index.php
@@ -105,7 +105,7 @@ const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
       tooltip: {
                 callbacks: {
                     title: function(tooltipItems, data){
-                        return tooltipItems.yLabel;
+                        return tooltipItems.xLabel;
                     },
                     label: function(context) {
                         var label = context.dataset.label || '';
