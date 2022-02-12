b3bd092ae436857df5317a2aad63b0638010887b
tamerelmenoufi <tamer@mohatron.com.br>
2022-02-11 22:07:26 -0400

    Incluir análise de logs

--- painel/paginas/cadastros/oficios/log.php
+++ painel/paginas/cadastros/oficios/log.php
@@ -15,7 +15,7 @@ if ($codigo) {
         right:35px;
     }
 </style>
-<div class="card shadow mb-4">
+<div class="card shadow mb-4" style="margin:20px;">
 
     <div class="card-body">
         <form id="form-oficios" enctype="multipart/form-data">
