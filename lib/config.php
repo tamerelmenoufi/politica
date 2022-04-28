1919f97ab30d2c8b6e71a3c42258ec2ffe698afb
tamerelmenoufi <tamer@mohatron.com.br>
2022-04-28 10:42:24 -0400

    Atualizações

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ function getUrl()
     if ($_SERVER['HTTP_HOST'] === 'localhost') return $protocol . "://localhost/dsv/politica/";
 
     #return $protocol . "://" . $_SERVER['HTTP_HOST'];
-    return 'http://http://3.93.20.163:8080/';
+    return 'http://politica.mohatron.com/';
 }
 
 $caminho_vendor = getUrl() . "lib/vendor";
