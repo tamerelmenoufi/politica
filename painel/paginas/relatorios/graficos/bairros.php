b28a9cdd1d70330973266097835c9113532da739
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-09 22:02:14 -0400

    Atualizações

--- painel/paginas/relatorios/graficos/bairros.php
+++ painel/paginas/relatorios/graficos/bairros.php
@@ -26,7 +26,7 @@ $Bd = [
 $query = "SELECT bai.descricao AS descricao, COUNT(*) AS qt FROM bairros bai "
     . "INNER JOIN beneficiados b ON b.bairro = bai.codigo "
     . "INNER JOIN servicos s ON s.beneficiado = b.codigo "
-    . "GROUP BY b.bairro";
+    . "GROUP BY bai.descricao";
 
 /*$query = "SELECT b.tipo, COUNT(*) AS qt FROM servicos a "
     . "LEFT JOIN servico_tipo b ON a.tipo = b.codigo GROUP BY a.tipo"
