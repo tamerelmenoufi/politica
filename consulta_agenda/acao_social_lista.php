c9d5b286da1902fa1533aefab7ec9935d3c2da1c
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-13 20:57:44 -0400

    Atualizações

--- consulta_agenda/acao_social_lista.php
+++ consulta_agenda/acao_social_lista.php
@@ -2,7 +2,6 @@
 
 include_once "../lib/includes.php";
 
-
 ?>
 
 <style>
@@ -24,14 +23,27 @@ include_once "../lib/includes.php";
                         </tr>
                     </thead>
                     <tbody>
+
+            <?php
+                    $query = "SELECT * FROM `acao_social` where data like '%{$_GET['data']}%'";
+                    $result = mysql_query($query);
+                    while($d = mysql_fetch_object($result)){
+
+                        $eventos[] = [
+                            'id' => $d->codigo,
+                            'title' => $d->local,
+                            'start' => substr($d->data, 0, 10),
+                        ];
+
+            ?>
+
                         <tr>
-                            <td>Multirão</td>
-                            <td>12/05/2022</td>
+                            <td><?=$d->local?></td>
+                            <td><?=$d->data?>  === <?=substr($d->data, 4,2)?>/<?=substr($d->data, 7,2)?>/<?=substr($d->data, 0,4)?></td>
                         </tr>
-                        <tr>
-                            <td>Cidade Nova</td>
-                            <td>26/05/2022</td>
-                        </tr>
+            <?php
+                    }
+            ?>
                     </tbody>
                 </table>
 
