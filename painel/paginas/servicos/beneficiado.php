b160e6eba4a241a3e100dbe3a9a727e0ed4ea68d
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-23 15:21:41 -0400

    Atualizações

--- painel/paginas/servicos/beneficiado.php
+++ painel/paginas/servicos/beneficiado.php
@@ -1,1 +1,10 @@
-Dados do bebeficiário
+<?php
+    include "../../../lib/includes.php";
+
+
+    $query = "SELECT * FROM beneficiados ORDER BY nome";
+    $result = mysql_query($query);
+
+    while ($b = mysql_fetch_object($result)): ?>
+        <p <?= $b->codigo?>><?= $b->nome; ?></p>
+<?php endwhile; ?>
