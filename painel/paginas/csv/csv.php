7334d72b139ecda3cbb4fad85ec5461874265c51
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-31 09:49:32 -0400

    Atualizações

--- painel/paginas/csv/csv.php
+++ painel/paginas/csv/csv.php
@@ -1,12 +1,14 @@
 <?php
 
-    include_once "config.php";
+    include_once "./config.php";
 
-    echo "<pre>";
-    print_r($_SESSION);
-    echo "</pre>";
-    echo "<hr>";
-    echo "<pre>";
-    print_r($_GET);
-    echo "</pre>";
+    echo "Acesso!";
+
+    // echo "<pre>";
+    // print_r($_SESSION);
+    // echo "</pre>";
+    // echo "<hr>";
+    // echo "<pre>";
+    // print_r($_GET);
+    // echo "</pre>";
 
