f01918f5fa308d1c3dad2a45b3b1798d63d53928
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-31 09:26:20 -0400

    Atualizações

--- painel/paginas/csv/config.php
+++ painel/paginas/csv/config.php
@@ -1,17 +1,2 @@
 <?php
 include "../../../lib/includes.php";
-
-function getSituacao()
-{
-    return [
-        'tramitacao' => 'Tramitação',
-        'retorno' => 'Retorno',
-        'concluido' => 'Concluído',
-    ];
-}
-
-function getSituacaoOptions($situacao)
-{
-    $list = getSituacao();
-    return $list[$situacao];
-}
