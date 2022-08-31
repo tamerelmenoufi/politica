fe7ffdf2328a5ef6d43beea946b8c4eb3f614921
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-31 10:07:18 -0400

    Atualizações

--- painel/paginas/csv/config_csv.php
+++ painel/paginas/csv/config_csv.php
@@ -1,2 +1,18 @@
 <?php
     include "../../../lib/includes.php";
+
+function getEsfera()
+{
+    return [
+        'Municipal', 'Estadual'
+    ];
+}
+
+function getSituacao()
+{
+    return [
+        'tramitacao' => 'Tramitação',
+        'retorno' => 'Retorno',
+        'concluido' => 'Concluído',
+    ];
+}
