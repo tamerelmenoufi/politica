fb0d61cef7d2d6b0f735bced70c447cec236a25e
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-27 09:52:25 -0400

    Atualizações

--- painel/paginas/servicos/saude/config_servicos.php
+++ painel/paginas/servicos/saude/config_servicos.php
@@ -18,7 +18,7 @@ function getEsfera()
 function getSituacao()
 {
     return [
-        '' => 'Limpar Filtro',
+        'o' => 'Limpar Filtro',
         'tramitacao' => 'Tramitação',
         'retorno' => 'Retorno',
         'concluido' => 'Concluído',
